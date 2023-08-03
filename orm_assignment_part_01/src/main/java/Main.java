387ea03143296506633f1666da0dc0d06894ced8
maheeshak <maheeshafernando18@gmail.com>
2023-08-03 19:46:34 +0530

    add save page

--- orm_assignment_part_01/src/main/java/Main.java
+++ orm_assignment_part_01/src/main/java/Main.java
@@ -63,6 +63,63 @@ public class Main {
                 continue;
             }
         }
+        private static void saveBook() {
+            Scanner input = new Scanner(System.in);
+            Integer option = 0;
+            while (option != -1) {
+
+                for (int i = 0; i < 25; i++) {
+                    System.out.print("-");
+
+                }
+                System.out.print("\n");
+                System.out.print("|");
+                System.out.print("\t\tSave Book");
+                System.out.println("\t\t|");
+
+                for (int i = 0; i < 25; i++) {
+                    System.out.print("-");
+                }
+
+                System.out.println("");
+
+
+                System.out.println("[01] Add Book");
+                System.out.println("[02] Main Page");
+                System.out.println("");
+                System.out.print("Please Select your option : ");
+                option = input.nextInt();
+                if (option == 1) {
+
+                    input.nextLine();
+                    System.out.print("Book ID : ");
+                    String book_id = input.nextLine();
+
+                    System.out.print("Book Title : ");
+                    String book_title = input.nextLine();
+
+                    System.out.print("Book ISBN : ");
+                    String book_isbn = input.nextLine();
+
+
+                    Book book = new Book(book_id, book_title, book_isbn);
+
+                    Boolean isValid = crudOperations(book, "isValid");
+                    if (isValid) {
+
+                        Boolean isSaved = crudOperations(book, "save");
+                        System.out.println("Book is Added !!!");
+                        System.out.println("");
+                    } else {
+                        System.out.println("Book ID is Already Added !!!");
+                        System.out.println("");
+                    }
+                } else {
+                    mainPage();
+                }
+            }
+
+        }
 
 
     }
