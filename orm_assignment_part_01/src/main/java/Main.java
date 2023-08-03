ee9f6d9ad86102eabdec17b6c192ab450ffc6f66
maheeshak <maheeshafernando18@gmail.com>
2023-08-03 19:49:27 +0530

    add update page

--- orm_assignment_part_01/src/main/java/Main.java
+++ orm_assignment_part_01/src/main/java/Main.java
@@ -7,6 +7,7 @@ public class Main {
         mainPage();
 
     }
+
     private static void mainPage() {
         Scanner input = new Scanner(System.in);
 
@@ -48,7 +49,7 @@ public class Main {
                 } else if (option == 3) {
                     viewBook();
 
-                }  else if (option == 4) {
+                } else if (option == 4) {
                     removeBook();
 
                 } else if (option == 5) {
@@ -63,7 +64,7 @@ public class Main {
                 continue;
             }
         }
-        private static void saveBook() {
+        private static void saveBook () {
             Scanner input = new Scanner(System.in);
             Integer option = 0;
             while (option != -1) {
@@ -117,6 +118,64 @@ public class Main {
                 } else {
                     mainPage();
                 }
+            }
+
+        }
+
+    }
+    private static void updateBook() {
+        Scanner input = new Scanner(System.in);
+        Integer option = 0;
+        while (option != -1) {
+
+            for (int i = 0; i < 25; i++) {
+                System.out.print("-");
+
+            }
+            System.out.print("\n");
+            System.out.print("|");
+            System.out.print("\t\tUpdate Book");
+            System.out.println("\t\t|");
+
+            for (int i = 0; i < 25; i++) {
+                System.out.print("-");
+            }
+
+            System.out.println("");
+
+
+            System.out.println("[01] Update Book");
+            System.out.println("[02] Main Page");
+            System.out.println("");
+            System.out.print("Please Select your option : ");
+            option = input.nextInt();
+            if (option == 1) {
+                input.nextLine();
+                System.out.print("Enter Book ID : ");
+                String book_id_new = input.nextLine();
+                Book book1 = new Book(book_id_new, "", "");
+                Boolean isValid1 = crudOperations(book1, "isValid");
+
+                if (!isValid1) {
+
+                    System.out.print("Book Title : ");
+                    String book_title = input.nextLine();
+
+                    System.out.print("Book ISBN : ");
+                    String book_isbn = input.nextLine();
+
+
+                    Book book = new Book(book_id_new, book_title, book_isbn);
+
+
+                    Boolean isSaved = crudOperations(book, "update");
+                    if (isSaved) {
+                        System.out.println("Book is Updated !!!");
+                        System.out.println("");
+                    }
+                }
+            } else {
+                mainPage();
             }
 
         }
