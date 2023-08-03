d52d41a5911468a1f7a2065af297a033464d2b89
maheeshak <maheeshafernando18@gmail.com>
2023-08-03 20:09:14 +0530

    update Main class

--- orm_assignment_part-02/one_to_one_relationship/src/main/java/Main.java
+++ orm_assignment_part-02/one_to_one_relationship/src/main/java/Main.java
@@ -1,5 +1,22 @@
+import entity.Author;
+import entity.Book;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import util.FactoryConfiguration;
+
 public class Main {
     public static void main(String[] args) {
+        Book book = new Book("B001","Madolduwa","ISBN-001");
+        Author author = new Author("A001", "Martin Wikramasinghe", "marting@gmail.com", book);
 
+
+        Session session = FactoryConfiguration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+
+        session.persist(book);
+        session.persist(author);
+
+        transaction.commit();
+        session.close();
     }
 }
