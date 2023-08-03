10f7f26c120f0d9e312eb6561071aaf60ee44bde
maheeshak <maheeshafernando18@gmail.com>
2023-08-03 20:41:47 +0530

    add Book class

---
+++ orm_assignment_part-02/many_to_many_relationships/src/main/java/entity/Book.java
@@ -0,0 +1,29 @@
+package entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+
+public class Book {
+
+    @Id
+    private String book_id;
+    private String book_title;
+    private String book_isbn;
+
+    @ManyToMany(mappedBy = "books")
+    private List<Author> authors;
+
+
+}
